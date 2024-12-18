c739ae6dedf4835cf6cbb8a53fa39a152e8536ca
sud0nymm <145041893+sud0nymm@users.noreply.github.com>
2024-12-18 20:55:10 +0100

    Update exerc3.cpp

--- exerc3.cpp
+++ exerc3.cpp
@@ -1,6 +1,6 @@
-/**************************
- * Lab 3: exercise 3      *
- **************************/
+/**
+ 
+Lab 3: exercise 3      ***/
 
 #include <map>
 #include <vector>
@@ -11,74 +11,60 @@
 using namespace std;
 
 bool containsWord(const vector<string> list, const string word);
-map<string, vector<string>> sortingMap(const vector<string> words);
 
 int main() {
 
-	std::ifstream file("C:/Users/ville/Desktop/tng033/lab3/code/uppgift3_kort.txt");
-	
-	if (file.is_open()) {
+    std::ifstream file("C:/Users/Hamilton/Desktop/TNG033/lab3/code/uppgift3_kort.txt");
 
-		vector<string> theWords;
-		std::string word;
+    if (file.is_open()) {
 
-		while (file >> word) {
+        vector<string> theWords;
+        std::string word;
+        std::map<std::string, std::vector<std::string>> anagramGroups;
 
-			if (theWords.size() == 0 || !containsWord(theWords, word)) {
-				theWords.push_back(word);
-			}
+        while (file >> word) {
 
-		}
+            if (theWords.size() == 0 || !containsWord(theWords, word)) {
+                theWords.push_back(word);
+            }
 
-		
+        }
 
-		//for (auto i : theWords)  cout << i << "\n";
+        for (const auto& theWord  : theWords) {
+            string subject = theWord;
+            std::sort(subject.begin(), subject.end());
+            anagramGroups[subject].push_back(theWord);
+        }
 
-		sortingMap(theWords);
+        for (auto it = anagramGroups.begin(); it != anagramGroups.end(); it++) {
+            if (it->second.size() < 2) {
+                it = anagramGroups.erase(it);
+                it--;
+            }
+        }
 
-	}
+        for (const auto& [subject, group] : anagramGroups) {
+            for (const auto& word : group) {
+                cout << word << " ";
+            }
+            cout << " --> "<< group.size() << " Words";
+            cout << endl;
+        }
 
-	return 0;
+
+
+    }
+
+    return 0;
 }
 
 bool containsWord(const vector<string> words, const string word) {
 
-	for (string s : words) {
-		if (s == word) {
-			return true;
-		}
-	}
+    for (string s : words) {
+        if (s == word) {
+            return true;
+        }
+    }
 
-	return false;
-}
-
-map<string, vector<string>> sortingMap(vector<string> words){
-
-	map<string, vector<string>> theList;
-	vector<string> sortedKeys;
-
-	for (std::string& word : words) {
-		std::sort(word.begin(), word.end());
-
-		if (!containsWord(sortedKeys, word)) {
-			sortedKeys.push_back(word);
-		}
-	}
-
-	for (auto i : sortedKeys)  cout << i << "\n";
-
-	/* for (int i = 0; i < words.size(); i++) {
-
-		string aWord = words[i];
-		sort(aWord.begin(), aWord.end());
-
-		if (!containsWord(sortedKeys, aWord)) {
-			;
-		}
-	} */
-
-	
-	
-
-	return theList;
+    return false;
 }
